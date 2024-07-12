7835fcb1c278ab1bc0637c651569fb3fb5db60cf
supriya gupta <supriyag75@gmail.com>
2024-07-12 13:30:10 +0530

    Optimize nav-drawer code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_navigation/views/nav_drawer_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,98 +16,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DrawerDemo extends StatefulWidget {
-  @override
-  State<DrawerDemo> createState() => _DrawerDemoState();
-}
 
-class _DrawerDemoState extends State<DrawerDemo> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Drawer Demo "),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            const UserAccountsDrawerHeader(
-              accountName: Text("Supriya Gupta"),
-              accountEmail: Text("supriyag75@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Text(
-                  "S",
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-              ),
-            ),
-            ListTile(
-              title: Row(
-                children: [
-                  Icon(Icons.home),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Text("Home"),
-                  ),
-
-                ],
-              ),
-            ),
-            ListTile(
-              title: Row(
-                children: [
-                  Icon(Icons.person),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Text("Profile"),
-                  ),
-
-                ],
-              ),
-            ),
-            ListTile(
-              title: Row(
-                children: [
-                  Icon(Icons.settings),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Text("Settings"),
-                  ),
-
-                ],
-              ),
-            ),
-            ListTile(
-              title: Row(
-                children: [
-                  Icon(Icons.contact_emergency),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Text("Contact"),
-                  ),
-
-                ],
-              ),
-            ),
-            ListTile(
-              title: Row(
-                children: [
-                  Icon(Icons.help),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Text("Help"),
-                  ),
-
-                ],
-              ),
-            )
-
-          ],
-        ),
-      ),
-    );
-  }
-}
